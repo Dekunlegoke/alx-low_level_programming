2733cbedc159905036c8a3158cba991af585689c
dekunlegoke <dekunlegoke@gmail.com>
2022-09-22 11:28:01 -0700

    my commit 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,23 @@
-nclude "main.h"
+int _putchar(char c);
 
-/**
- * _isupper -checks if a character is uppercase or not
- * @c: character to be tested
- * Retur: 1 whether it is, 0 otherwise
- */
+int _isupper(int c);
 
-int_isupper(int c)
-{
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		return (1);
-	}
+int _isdigit(int c);
 
-	return (0);
-}
+int mul(int a, int b);
+
+void print_number(int n);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
