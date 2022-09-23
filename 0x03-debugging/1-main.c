cf29602776b031d2ff56ad260b8ac9b85dfc886a
dekunlegoke <dekunlegoke@gmail.com>
2022-09-23 10:08:57 -0700

    my commit 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,9 +9,7 @@
 int main(void)
 {
 		int i;
-
 		printf("Infinite loop incoming :(\n");
-
 		i = 0;
 
 		/*while (i < 10)*/
@@ -22,10 +20,8 @@ int main(void)
 
 		/*}*/
 
-
 		printf("Infinite loop avoided! \\o/\n");
 
-
-		return (0);
+	return (0);
 
 }
