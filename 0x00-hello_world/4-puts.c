23edef32c877784ee4f9679a2caee0b9a479e466
dekunlegoke <dekunlegoke@gmail.com>
2022-09-23 11:00:54 -0700

    my commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - entry point
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- * Return: 0 after printing the function
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
